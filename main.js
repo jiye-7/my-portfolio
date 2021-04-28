8a80e4b9c09e39123ec621f76094145dbfa84eb6
jiye-7 <jy522hi@gmail.com>
2021-04-29 00:51:10 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -2,28 +2,20 @@
 
 /*
   1. navbar 구현해야 될 것
-    해당 메뉴 클릭시, 해당 위치로 이동 --> 스크롤 이벤트
-
+    1-1. 화면 맨 위에서 scroll시 navbar backgroun-color 변경, font-color 변경
+    1-2. 해당 메뉴 클릭시, 해당 위치로 이동 --> 스크롤 이벤트
 */
 
 const navbar = document.querySelector('#navbar');
-const menuNavbar = document.querySelector('.navbar__menu');
-const menus = document.querySelectorAll('.navbar__menu__item');
-const menuHome = document.querySelector('.menu__home');
-const menuAbout = document.querySelector('.menu__about');
-const menuSkills = document.querySelector('.menu__skills');
-const menuWork = document.querySelector('.menu__work');
-const menuTestimonial = document.querySelector('.menu__testimonial');
+const navbarMenu = document.querySelector('.navbar__menu');
 const menuContact = document.querySelector('.menu__contact');
 
 const work = document.querySelector('#work');
 
-// Make navbar transparent when it is on the top
+/* Make navbar transparent when it is on the top */
 document.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
   const menuY = navbar.getBoundingClientRect().height;
-  console.log(scrollY);
-  console.log(menuY);
 
   if (scrollY > menuY) {
     navbar.classList.add('navbar--dark');
@@ -34,3 +26,20 @@ document.addEventListener('scroll', () => {
     // navbar.style.backgroundColor = 'transparent';
   }
 });
+
+/* Handle scrolling when tapping on the navbar menu */
+let handleMoveMenu = (e) => {
+  e.preventDefault();
+  const menuLi = e.target;
+  console.log(menuLi);
+
+  if (menuLi.classList.contains('navbar__menu__item')) { //menuLi.nodeName === 'LI'
+    let menuPosition = menuLi.dataset.link;
+    let target = document.querySelector(`${menuPosition}`);
+    console.log(target);
+    target.scrollIntoView({ 'behavior': 'smooth' });
+  }
+}
+
+navbarMenu.addEventListener('click', handleMoveMenu);
+navbarMenu.addEventListener('click', handleMoveMenu);
